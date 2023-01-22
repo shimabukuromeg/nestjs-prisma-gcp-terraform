dcfee03726df0d1821194b20507f6f86c52cb59e
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2023-01-22 23:38:09 +0900

    repository名にランダムな値を渡すようにした

--- modules/artifact_registry/main.tf
+++ modules/artifact_registry/main.tf
@@ -2,7 +2,11 @@
 resource "google_artifact_registry_repository" "my-repo" {
   project       = var.project_id
   location      = var.location
-  repository_id = var.repository_id
+  repository_id = "${var.repository_id}-${random_id.repository_id_suffix.hex}"
   description   = "example docker repository"
   format        = "DOCKER"
+}
+
+resource "random_id" "repository_id_suffix" {
+  byte_length = 4
 }
